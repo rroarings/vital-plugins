30f01d1027a89d2f45fac12c13780bede11fc861
Vitalflea <vitalflea@arceneaux.club>
2022-08-03 08:38:09 -0700

    vital-quester-0.0.6 handle quest completed widget

--- vital-quester/src/main/java/dev/vital/quester/tasks/HandleQuestComplete.java
+++ vital-quester/src/main/java/dev/vital/quester/tasks/HandleQuestComplete.java
@@ -3,7 +3,11 @@ package dev.vital.quester.tasks;
 import dev.vital.quester.ScriptTask;
 import dev.vital.quester.VitalQuesterConfig;
 import net.runelite.api.widgets.WidgetInfo;
+import net.unethicalite.api.input.Keyboard;
 import net.unethicalite.api.widgets.Widgets;
+
+import static java.awt.event.KeyEvent.VK_ESCAPE;
+import static net.runelite.api.widgets.WidgetInfo.QUEST_COMPLETED;
 
 public class HandleQuestComplete implements ScriptTask
 {
@@ -12,15 +16,18 @@ public class HandleQuestComplete implements ScriptTask
     public HandleQuestComplete(VitalQuesterConfig config) {
         this.config = config;
     }
+
     @Override
     public boolean validate()
     {
-        var widget = Widgets.get(WidgetInfo.QUEST_COMPLETED);
-        return widget != null && widget.isVisible();
+        var quest_completed_x = Widgets.get(QUEST_COMPLETED);
+        return quest_completed_x != null && !quest_completed_x.isHidden();
     }
 
     @Override
     public int execute() {
+
+        Widgets.get(153, 16).interact("Close");
 
         return -1;
     }

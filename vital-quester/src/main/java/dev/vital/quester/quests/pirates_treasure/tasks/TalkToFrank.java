adf6cf33c88085f3d98b0d21144d608d6b5a3d2e
Vitalflea <vitalflea@arceneaux.club>
2022-08-12 10:28:02 -0700

    Remove unused imports

--- vital-quester/src/main/java/dev/vital/quester/quests/pirates_treasure/tasks/TalkToFrank.java
+++ vital-quester/src/main/java/dev/vital/quester/quests/pirates_treasure/tasks/TalkToFrank.java
@@ -1,16 +1,11 @@
 package dev.vital.quester.quests.pirates_treasure.tasks;
 
 import dev.vital.quester.DialogTask;
-import dev.vital.quester.QuestList;
 import dev.vital.quester.ScriptTask;
 import dev.vital.quester.VitalQuesterConfig;
-import dev.vital.quester.tools.Tools;
 import net.runelite.api.ItemID;
-import net.runelite.api.Quest;
-import net.runelite.api.QuestState;
 import net.runelite.api.coords.WorldPoint;
 import net.unethicalite.api.items.Inventory;
-import net.unethicalite.api.quests.Quests;
 
 public class TalkToFrank implements ScriptTask
 {

36b3d486b3d66c710ab6d7b2e8f4397ccfc3c0e3
Vitalflea <vitalflea@arceneaux.club>
2022-08-14 02:45:36 -0700

    vital-quester-0.1.17 Change to "misc" and add skill combo

---
+++ vital-quester/src/main/java/dev/vital/quester/ui/AboutContainer.java
@@ -0,0 +1,32 @@
+package dev.vital.quester.ui;
+
+import dev.vital.quester.VitalQuester;
+import dev.vital.quester.VitalQuesterConfig;
+import net.runelite.client.config.ConfigManager;
+import net.runelite.client.util.LinkBrowser;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class AboutContainer extends PanelContainer
+{
+    public AboutContainer(VitalQuesterConfig config, ConfigManager configManager)
+    {
+        super("About", config, configManager);
+    }
+
+    @Override
+    protected void rebuild()
+    {
+        removeAll();
+
+        JButton button = new JButton("Version " + VitalQuester.version);
+        button.setSize(200, 20);
+        button.setMinimumSize(new Dimension(200,20));
+        button.addActionListener(e -> LinkBrowser.browse("https://github.com/Vitalflea/vital-plugins/tree/development/vital-quester"));
+
+        add(button);
+
+        revalidate();
+    }
+}

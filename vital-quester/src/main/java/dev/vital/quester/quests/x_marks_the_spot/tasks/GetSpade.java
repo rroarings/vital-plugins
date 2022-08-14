6a9d18609a0bf1be06f2d174f156b76a0b242f25
Vitalflea <vitalflea@arceneaux.club>
2022-08-14 16:39:09 -0700

    rename to spade

--- vital-quester/src/main/java/dev/vital/quester/quests/x_marks_the_spot/tasks/GetSpade.java
+++ vital-quester/src/main/java/dev/vital/quester/quests/x_marks_the_spot/tasks/GetSpade.java
@@ -1,5 +1,6 @@
 package dev.vital.quester.quests.x_marks_the_spot.tasks;
 
+import dev.vital.quester.BasicTask;
 import dev.vital.quester.ScriptTask;
 import dev.vital.quester.VitalQuesterConfig;
 import dev.vital.quester.tools.Tools;
@@ -7,13 +8,13 @@ import net.runelite.api.ItemID;
 import net.runelite.api.coords.WorldPoint;
 import net.unethicalite.api.items.Inventory;
 
-public class GetShovel implements ScriptTask
+public class GetSpade implements ScriptTask
 {
     private final WorldPoint shop_keeper_point = new WorldPoint(3213, 3247, 0);
 
     VitalQuesterConfig config;
 
-    public GetShovel(VitalQuesterConfig config)
+    public GetSpade(VitalQuesterConfig config)
     {
         this.config = config;
     }
@@ -23,9 +24,18 @@ public class GetShovel implements ScriptTask
     {
         return !Inventory.contains(ItemID.SPADE);
     }
+    BasicTask sell_bronze_axe = new BasicTask(() ->
+            Tools.sellTo("Shop keeper", shop_keeper_point, ItemID.BRONZE_AXE, 0, false));
 
     @Override
     public int execute() {
+
+        if(Inventory.getCount(true, ItemID.COINS_995) < 3 && Inventory.contains(ItemID.BRONZE_AXE)) {
+
+            if (!sell_bronze_axe.taskCompleted()) {
+                return sell_bronze_axe.execute();
+            }
+        }
 
         if(!Inventory.contains(ItemID.SPADE) && Inventory.getCount(true, ItemID.COINS_995) >= 3) {
             return Tools.purchaseFrom("Shop keeper", shop_keeper_point, ItemID.SPADE, 1, false);

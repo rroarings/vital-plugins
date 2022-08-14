629d44a551312d23395f8ece629646cc8bc7d08c
Vitalflea <vitalflea@arceneaux.club>
2022-08-14 16:47:44 -0700

    add talk to romeo2 task

---
+++ vital-quester/src/main/java/dev/vital/quester/quests/romeo_and_juliet/tasks/TalkToRomeo2.java
@@ -0,0 +1,39 @@
+package dev.vital.quester.quests.romeo_and_juliet.tasks;
+
+import dev.vital.quester.DialogTask;
+import dev.vital.quester.ScriptTask;
+import dev.vital.quester.VitalQuesterConfig;
+import net.runelite.api.coords.WorldPoint;
+import net.unethicalite.api.game.Vars;
+import net.unethicalite.api.quests.QuestVarPlayer;
+
+public class TalkToRomeo2 implements ScriptTask
+{
+    private final WorldPoint romeo_point = new WorldPoint(3215, 3418, 0);
+
+    VitalQuesterConfig config;
+
+    public TalkToRomeo2(VitalQuesterConfig config)
+    {
+        this.config = config;
+    }
+
+    @Override
+    public boolean validate()
+    {
+        return Vars.getBit(QuestVarPlayer.QUEST_ROMEO_AND_JULIET.getId()) == 60;
+    }
+
+    DialogTask talk_to_romeo = new DialogTask("Romeo",  romeo_point,
+            (String)null);
+
+    @Override
+    public int execute() {
+
+        if (!talk_to_romeo.taskCompleted()) {
+            return talk_to_romeo.execute();
+        }
+
+        return -1;
+    }
+}

63b4e30c8cdf33dd011a1153a879136ffa0c06f3
Vitalflea <vitalflea@arceneaux.club>
2022-08-14 02:43:33 -0700

    vital-quester-0.1.17 Change to "misc" and add skill combo

--- vital-quester/src/main/java/dev/vital/quester/ui/MiscellaneousContainer.java
+++ vital-quester/src/main/java/dev/vital/quester/ui/MiscellaneousContainer.java
@@ -1,13 +1,14 @@
 package dev.vital.quester.ui;
 
 import dev.vital.quester.VitalQuesterConfig;
+import net.runelite.api.Skill;
 import net.runelite.client.config.ConfigManager;
 
 public class MiscellaneousContainer extends PanelContainer
 {
     public MiscellaneousContainer(VitalQuesterConfig config, ConfigManager configManager)
     {
-        super("Miscellaneous", config, configManager);
+        super("Misc", config, configManager);
     }
 
     @Override
@@ -17,6 +18,10 @@ public class MiscellaneousContainer extends PanelContainer
 
         add(createCheckBox("Handle genie", "handleGenie"), "wrap");
         add(createCheckBox("Handle lamps", "handleLamp"), "wrap");
+
+        if(Boolean.parseBoolean(configManager.getConfiguration(VitalQuesterConfig.CONFIG_GROUP, "handleLamp"))) {
+            add(createComboBoxSection("Skill", "lampSkill", Skill.class), "wrap");
+        }
 
         revalidate();
     }

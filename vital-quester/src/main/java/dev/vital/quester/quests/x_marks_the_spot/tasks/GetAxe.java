bc6096824251d58a9c34b27b05b4c2a9f3d1ddea
Vitalflea <vitalflea@arceneaux.club>
2022-08-14 16:42:57 -0700

    add getting an axe during x marks the spot

---
+++ vital-quester/src/main/java/dev/vital/quester/quests/x_marks_the_spot/tasks/GetAxe.java
@@ -0,0 +1,38 @@
+package dev.vital.quester.quests.x_marks_the_spot.tasks;
+
+import dev.vital.quester.DialogTask;
+import dev.vital.quester.ScriptTask;
+import dev.vital.quester.VitalQuesterConfig;
+import net.runelite.api.ItemID;
+import net.runelite.api.coords.WorldPoint;
+import net.unethicalite.api.items.Inventory;
+
+public class GetAxe implements ScriptTask
+{
+    private final WorldPoint shop_keeper_point = new WorldPoint(3227, 3244, 0);
+
+    VitalQuesterConfig config;
+
+    public GetAxe(VitalQuesterConfig config)
+    {
+        this.config = config;
+    }
+
+    @Override
+    public boolean validate()
+    {
+        return !Inventory.contains(ItemID.BRONZE_AXE) && !Inventory.contains(ItemID.SPADE);
+    }
+
+    DialogTask talk_to_wood_guy = new DialogTask("Woodsman tutor", shop_keeper_point, "Can you teach me");
+
+    @Override
+    public int execute() {
+
+        if (!talk_to_wood_guy.taskCompleted()) {
+            return talk_to_wood_guy.execute();
+        }
+
+        return -2;
+    }
+}

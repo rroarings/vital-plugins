ad1e31838a3be349c106d2e8200a0da9b04e04ed
Vitalflea <vitalflea@arceneaux.club>
2022-08-14 12:03:23 -0700

    add CameraTask

---
+++ vital-quester/src/main/java/dev/vital/quester/CameraTask.java
@@ -0,0 +1,41 @@
+package dev.vital.quester;
+
+import net.unethicalite.api.commons.Time;
+import net.unethicalite.api.input.Keyboard;
+
+import static java.awt.event.KeyEvent.VK_LEFT;
+import static java.awt.event.KeyEvent.VK_RIGHT;
+
+public class CameraTask {
+
+    boolean task_completed;
+    int sleep_amount;
+
+    public CameraTask(int sleep_amount) {
+        this.task_completed = false;
+        this.sleep_amount = sleep_amount;
+    }
+
+    public void moveRight() {
+
+        Keyboard.pressed(VK_RIGHT);
+        Time.sleepTicks(this.sleep_amount);
+        Keyboard.released(VK_RIGHT);
+
+        this.task_completed = true;
+    }
+
+    public void moveLeft() {
+
+        Keyboard.pressed(VK_LEFT);
+        Time.sleepTicks(this.sleep_amount);
+        Keyboard.released(VK_LEFT);
+
+        this.task_completed = true;
+    }
+
+    public boolean taskCompleted() {
+        return this.task_completed;
+    }
+}
+

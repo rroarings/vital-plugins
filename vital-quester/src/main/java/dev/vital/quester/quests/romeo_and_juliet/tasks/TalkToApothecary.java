5c69058904258f5af2791b39216610468a1c6645
Vitalflea <vitalflea@arceneaux.club>
2022-08-15 11:01:14 -0700

    avoid scary wizards

--- vital-quester/src/main/java/dev/vital/quester/quests/romeo_and_juliet/tasks/TalkToApothecary.java
+++ vital-quester/src/main/java/dev/vital/quester/quests/romeo_and_juliet/tasks/TalkToApothecary.java
@@ -1,15 +1,16 @@
 package dev.vital.quester.quests.romeo_and_juliet.tasks;
 
-import dev.vital.quester.DialogTask;
+import dev.vital.quester.tasks.DialogTask;
 import dev.vital.quester.ScriptTask;
 import dev.vital.quester.VitalQuesterConfig;
+import dev.vital.quester.tasks.WalkTask;
 import net.runelite.api.coords.WorldPoint;
 import net.unethicalite.api.game.Vars;
 import net.unethicalite.api.quests.QuestVarPlayer;
 
 public class TalkToApothecary implements ScriptTask
 {
-    private final WorldPoint apothecary_point = new WorldPoint(3254, 3497, 0);
+    private final WorldPoint apothecary_point = new WorldPoint(3194, 3403, 0);
 
     VitalQuesterConfig config;
 
@@ -21,16 +22,20 @@ public class TalkToApothecary implements ScriptTask
     @Override
     public boolean validate()
     {
-        return Vars.getBit(QuestVarPlayer.QUEST_ROMEO_AND_JULIET.getId()) == 40;
+        return Vars.getVarp(QuestVarPlayer.QUEST_ROMEO_AND_JULIET.getId()) == 40;
     }
 
-    DialogTask talk_to_apothecary = new DialogTask("Apothecary", apothecary_point.getWorldLocation(),
+    DialogTask talk_to_apothecary = new DialogTask("Apothecary", apothecary_point,
             "Talk about something else.", "Talk about Romeo & Juliet.");
+    WalkTask walk_task1 = new WalkTask(new WorldPoint(3283, 3428, 0));
 
     @Override
     public int execute() {
 
-        if (!talk_to_apothecary.taskCompleted()) {
+        if (!walk_task1.taskCompleted()) {
+            return walk_task1.execute();
+        }
+        else if (!talk_to_apothecary.taskCompleted()) {
             return talk_to_apothecary.execute();
         }
 
